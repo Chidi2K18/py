c3026b27321bf4724a0c246689288d2796f8ca35
nathanael mosaku <37617151+Chidi2K18@users.noreply.github.com>
2023-07-22 19:25:17 +0100

    added documentation

--- website/__init__.py
+++ website/__init__.py
@@ -1,44 +1,68 @@
+# Import necessary modules
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+# Initialize the SQLAlchemy database object
 db = SQLAlchemy()
+
+# Set the name of the SQLite database file
 DB_NAME = "database.db"
 
-
+# Function to create and configure the Flask app
 def create_app():
+    # Create the Flask app
     app = Flask(__name__)
+    
+    # Set the secret key for the app (used for session security)
     app.config['SECRET_KEY'] = 'This is my new key'
+    
+    # Set the URI for the SQLite database
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    
+    # Initialize the database with the Flask app
     db.init_app(app)
 
+    # Import the views and auth blueprints (routes) for the app
     from .views import views
     from .auth import auth
 
+    # Register the blueprints with the app and define their URL prefixes
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # Import the Influencer and Business models
     from .models import Influencer, Business
     
+    # Create the database tables using the models
     with app.app_context():
         db.create_all()
 
+    # Initialize the LoginManager for handling user authentication
     login_manager = LoginManager()
+    
+    # Set the login view for authentication (used for redirects)
     login_manager.login_view = 'auth.login_i'
     login_manager.init_app(app)
 
+    # Define the user loaders for the LoginManager to retrieve user objects
     @login_manager.user_loader
     def load_Influencer(id):
         return Influencer.query.get(int(id))
+    
     @login_manager.user_loader
     def load_Business(id):
         return Business.query.get(int(id))
 
+    # Return the configured Flask app
     return app
 
 
+# Function to create the database (if it does not exist)
 def create_database(app):
+    # Check if the database file does not exist
     if not path.exists('website/' + DB_NAME):
+        # Create all the database tables using the app context
         db.create_all(app=app)
         print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -4,29 +4,38 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db   ##means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
+# Create a Blueprint for the 'auth' module to handle user authentication related routes
 auth = Blueprint('auth', __name__)
 
-# Login route for Influencer
+# Route to handle login for Influencers
 @auth.route('/loginI', methods=['GET', 'POST'])
 def login_i():
+    # Check if the request method is POST (form submission)
     if request.method == 'POST':
+        # Get the input values from the login form
         email = request.form.get('email')
         password = request.form.get('password')
 
+        # Attempt to find an Influencer with the provided email in the database
         user = Influencer.query.filter_by(email=email).first()
         if user:
+            # Check if the provided password matches the hashed password stored in the database
             if check_password_hash(user.password, password):
+                # If the password is correct, log the user in, display a success message, and redirect to the home page
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
+                # If the password is incorrect, display an error message
                 flash('Incorrect password, try again.', category='error')
         else:
+            # If no Influencer with the provided email exists, display an error message
             flash('Email does not exist.', category='error')
 
+    # Render the login template, passing the current_user object (used for handling user sessions)
     return render_template("login.html", user=current_user)
 
-# Login route for Business
+# Route to handle login for Businesses
 @auth.route('/loginB', methods=['GET', 'POST'])
 def login_b():
     if request.method == 'POST':
@@ -46,34 +55,45 @@ def login_b():
 
     return render_template("login.html", user=current_user)
 
-# Logout route
+# Route to handle user logout (authentication required)
 @auth.route('/logout')
 @login_required
 def logout():
+    # Log the user out, remove their session, and redirect to the home page
     logout_user()
     return redirect(url_for('views.home'))
 
-# Sign up route for Influencer
+# Route to handle sign up for Influencers
 @auth.route('/signupi', methods=['GET', 'POST'])
 def sign_up_i():
+    # Check if the request method is POST (form submission)
     if request.method == 'POST':
+        # Get the input values from the signup form
         email = request.form.get('email')
         first_name = request.form.get('firstName')
         password1 = request.form.get('password')
         password2 = request.form.get('passwordC')
 
+        # Attempt to find an Influencer with the provided email in the database
         user = Influencer.query.filter_by(email=email).first()
         if user:
+            # If an Influencer with the provided email already exists, display an error message
             flash('Email already exists.', category='error')
         elif len(email) < 4:
+            # If the email is too short, display an error message
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
+            # If the first name is too short, display an error message
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
+            # If the provided passwords don't match, display an error message
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
+            # If the password is too short, display an error message
             flash('Password must be at least 7 characters.', category='error')
         else:
+            # If all validation checks pass, create a new Influencer, add it to the database, log them in,
+            # display a success message, and redirect to the home page
             new_user = Influencer(email=email, first_name=first_name, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
@@ -82,9 +102,10 @@ def sign_up_i():
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
+    # Render the signup template, passing the current_user object (used for handling user sessions)
     return render_template("signupi.html", user=current_user)
 
-# Sign up route for Business
+# Route to handle sign up for Businesses
 @auth.route('/signupb', methods=['GET', 'POST'])
 def sign_up_b():
     if request.method == 'POST':
